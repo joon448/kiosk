8a96b64280fb8249c8e74367df64a5d45273967e
joon448 <joonjdy448@gmail.com>
2025-07-16 19:13:06 +0900

    Feat: Lv6: Cart 및 CartItem 클래스 구현

--- src/lv6/Cart.java
+++ src/lv6/Cart.java
@@ -1,4 +1,36 @@
 package lv6;
 
+import java.util.List;
+
 public class Cart {
+    private List<MenuItem> cartItems;
+
+    public List<MenuItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<MenuItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public void addToCart(MenuItem menuItem) {
+        cartItems.add(menuItem);
+    }
+
+    public void removeFromCart(MenuItem menuItem) {
+        cartItems.remove(menuItem);
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
+
+    public void printCart() {
+        System.out.println("[ CART ]");
+        for (int i = 0; i < cartItems.size(); i++) {
+            MenuItem menuItem = cartItems.get(i);
+            System.out.printf("%-2d. %-15s | ₩ %-6d | %s%n", i+1 , menuItem.getName(), menuItem.getPrice(), menuItem.getDetail());
+        }
+    }
 }
+

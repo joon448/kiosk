b2b92c97a0881a490cf265a2afc2c17896cc6702
joon448 <joonjdy448@gmail.com>
2025-07-16 13:54:57 +0900

    Feat: Lv1 키오스크 메뉴 출력, 입력 및 주문 처리 기능 구현

--- src/lv1/Main.java
+++ src/lv1/Main.java
@@ -1,17 +1,45 @@
 package lv1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        boolean flag = true;
+        while (flag){
+            System.out.print("[ SHAKESHACK MENU ]\n"+
+                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+                    "0. 종료          | 종료\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            num = scanner.nextInt();
+            switch (num){
+                case 1:
+                    System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                    System.out.println("주문 완료되었습니다.\n");
+                    break;
+                case 2:
+                    System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                    System.out.println("주문 완료되었습니다.\n");
+                    break;
+                case 3:
+                    System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                    System.out.println("주문 완료되었습니다.\n");
+                    break;
+                case 4:
+                    System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                    System.out.println("주문 완료되었습니다.\n");
+                    break;
+                case 0:
+                    flag = false;
+                    System.out.println("프로그램을 종료합니다.\n");
+                    break;
+                default:
+                    System.out.println("잘못된 입력입니다.\n");
+            }
         }
     }
 }
